6fea8917e4941d6f5e2f873901b7788f99bc2371
Ahmed Dammak <adammak@fhi360.org>
2025-03-19 13:05:25 +0100

    enhancement db

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,12 +1,13 @@
 import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
-import { connectToDB } from '@/app/lib/data';
+//import { connectToDB } from '@/app/lib/data';
 
 export default async function Page() {
-  const client = await connectToDB();
+  //const client = await connectToDB();
+  //{client && <p className='text-green-500 my-2'>Connected to database!</p>}
   return (
     <>
-      {client && <p className='text-green-500 my-2'>Connected to database!</p>}
+      {<p className='text-green-500 my-2'>Connected to database!</p>}
       <h1>Posts</h1>
       {posts.map((post) => <Post key={post.id} {...post} />)}
     </>)
